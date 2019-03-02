620f3183de26b7acc51c03e886c73ebcd070642d
Hetal Dave <Hetal.Dave@verndale.com>
2019-03-02 14:46:31 -0500

    Foundation.Models project.

---
+++ src/Foundation/Models/code/Foundation.Models/Custom/Product_Reviews.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Foundation.Models.Custom
+{
+    public partial class Product_Reviews : GlassBase, IProduct_Reviews
+    {
+        [SitecoreChildren(InferType = true)]
+        public virtual IEnumerable<Review> ReviewsOfProduct { get; set; }
+
+        public string Review_Description { get; set; }
+        public string Reviews_Title { get; set; }
+    }
+    
+}
