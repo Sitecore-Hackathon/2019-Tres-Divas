4f6fdbcb4e22b1be2db5593935e485e04be76067
Deepthi Katta <deepthi.katta@verndale.com>
2019-03-01 20:09:27 -0800

    Checking in initial scaffolding, TDS Global, Code gen, sample stuff

---
+++ src/Project/Website/code/TresDivas.Website/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TresDivas.Website.Controllers
+{
+    public class ProductController : GlassController
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        protected static class Views
+        {
+            /// <summary>
+            /// FAQ Detail View
+            /// </summary>
+            public const string Detail = "~/Views/Product/ProductDetail.cshtml";
+        }
+
+    }
+
+   
+}
